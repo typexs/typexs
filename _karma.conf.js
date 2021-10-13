d01eddccef35f2c7495e2528d8d56dd889302ea1
Robert Kania <cezaryrk@yahoo.de>
2021-10-13 20:02:17 +0200

    test: rename karma to prevent

---
+++ _karma.conf.js
@@ -0,0 +1,40 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    files: [
+      { pattern: 'src/*.spec.ts', watched: true, included: true, served: true },
+      { pattern: 'src/**/*.spec.ts', watched: true, included: true, served: true }
+    ],
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    coverage: {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      dir: require('path').join(__dirname, './coverage/karma'),
+      reports: ['html', 'lcovonly', 'text-summary'],
+      fixWebpackSourcePaths: true
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['ChromeHeadless'],
+    singleRun: false,
+    restartOnFileChange: true
+  });
+};
+
+
