732e576533c0790afca0b2e3efc664568e075130
Robert Kania <cezaryrk@yahoo.de>
2022-03-25 09:31:10 +0100

    fix: update public api

--- packages/search-ng/src/public_api.ts
+++ packages/search-ng/src/public_api.ts
@@ -3,6 +3,7 @@ export { SearchEmbeddedComponent } from './components/embedded/embedded.componen
 export { SearchQueryFormComponent } from './components/query-form/query-form.component';
 export { QueryAction, QUERY_MODE } from './components/query-form/QueryAction';
 export { SearchFacetComponent } from './components/facet/facet.component';
+export { SearchFacetsComponent } from './components/facets/facets.component';
 
 // export {IStorageOptions} from './'
 export { SearchModule } from './module';
