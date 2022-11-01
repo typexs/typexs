24aa99f5e0292c4c418cb409f05a62da7ff180d6
Robert Kania <cezaryrk@yahoo.de>
2022-11-01 22:25:16 +0100

    fix: comments

--- packages/pipelines/src/lib/IPipelineProcessor.ts
+++ packages/pipelines/src/lib/IPipelineProcessor.ts
@@ -1,10 +1,13 @@
-import {ClassType} from '@allgemein/schema-api';
-import {Processor} from './Processor';
-import {IPipelineEntry} from './IPipelineEntry';
+import { ClassType } from '@allgemein/schema-api';
+import { Processor } from './Processor';
+import { IPipelineEntry } from './IPipelineEntry';
 import { IProcessorOptions } from './processor/IProcessorOptions';
 
 export interface IPipelineProcessor extends IPipelineEntry {
   processor: {
+    /**
+     * Name or ClassType of processing object
+     */
     name: string | ClassType<Processor>;
     options?: IProcessorOptions;
   };
