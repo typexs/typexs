2bfef2f4b5016a2762784520fe1cc2fbde9a8ea5
Robert Kania <cezaryrk@yahoo.de>
2025-02-14 21:14:41 +0100

    feat: upgrade to ng 18

--- packages/search-ng/src/components/facet/facet.component.spec.ts
+++ packages/search-ng/src/components/facet/facet.component.spec.ts
@@ -1,15 +1,15 @@
-import { async, TestBed } from '@angular/core/testing';
+import { waitForAsync, TestBed } from '@angular/core/testing';
 import { SearchFacetComponent } from './facet.component';
 
 describe('SearchFacetComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [
         SearchFacetComponent
       ]
     }).compileComponents();
   }));
-  it('should create the facet', async(() => {
+  it('should create the facet', waitForAsync(() => {
     const fixture = TestBed.createComponent(SearchFacetComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
